b42ebf2d828cb14d9fd5554851a986f32a2f57df
Degarashi <degarasu@gmail.com>
2025-09-16 19:12:22 +0900

    [update] ThighDir: add_optional_arguments_to_parser()は共通関数を使用

--- src/thigh_dir.py
+++ src/thigh_dir.py
@@ -9,7 +9,7 @@ from common.serialize import vec_serialize
 from common.types import TableDef
 from common.vec_db import VecDb
 from desc.thigh_dir import Table_Def, init_table_query
-from torsodir_db import add_optional_arguments_to_parser
+from common.db_readwrite import add_optional_arguments_to_parser
 
 
 class ThighDirDB(VecDb):
